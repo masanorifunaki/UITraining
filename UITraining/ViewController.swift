115990af0715883ca78fb5b70b953b4cebbc59db
舟木正憲 <funakimfunaki@gmail.com>
2018-12-28 14:57:22 +0900

    first commit

---
+++ UITraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITraining
+//
+//  Created by 舟木正憲 on 2018/12/28.
+//  Copyright © 2018 masanori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
